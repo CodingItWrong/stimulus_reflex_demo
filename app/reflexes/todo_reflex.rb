c7601956bb96d81e8dc8cfa700758aa40c4f0dbe
Josh Justice <me@codingitwrong.com>
2020-09-13 08:45:32 -0400

    Save editing todo ID in session

--- app/reflexes/todo_reflex.rb
+++ app/reflexes/todo_reflex.rb
@@ -1,17 +1,22 @@
 class TodoReflex < ApplicationReflex
+  after_reflex :set_editing_todo_id_instance_var
+
   def create
     Todo.create!(todo_params)
   end
 
   def edit
-    @editing_id = todo_id
+    self.editing_todo_id = todo_id
   end
 
   def cancel_edit
+    self.editing_todo_id = nil
   end
 
   def update
-    Todo.find(todo_id).update!(todo_params)
+    Todo.find(editing_todo_id)
+        .update!(todo_params)
+    self.editing_todo_id = nil
   end
 
   def delete
@@ -19,6 +24,18 @@ class TodoReflex < ApplicationReflex
   end
 
   private
+
+  def editing_todo_id
+    session[:editing_todo_id]
+  end
+
+  def editing_todo_id=(id)
+    session[:editing_todo_id] = id
+  end
+
+  def set_editing_todo_id_instance_var
+    @editing_id = editing_todo_id
+  end
 
   def todo_id
     element.dataset[:id].to_i
