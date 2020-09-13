93faba8c2deaaac373eadc1b495e6597fedcd7af
Josh Justice <me@codingitwrong.com>
2020-09-13 08:37:34 -0400

    Edit and cancel edit

--- app/reflexes/todo_reflex.rb
+++ app/reflexes/todo_reflex.rb
@@ -3,12 +3,26 @@ class TodoReflex < ApplicationReflex
     Todo.create!(todo_params)
   end
 
+  def edit
+    @editing_id = todo_id
+  end
+
+  def cancel_edit
+  end
+
+  def update
+    Todo.find(todo_id).update!(todo_params)
+  end
+
   def delete
-    id = element.dataset[:id]
-    Todo.find(id).destroy!
+    Todo.find(todo_id).destroy!
   end
 
   private
+
+  def todo_id
+    element.dataset[:id].to_i
+  end
 
   def todo_params
     params.require(:todo).permit(:name)
