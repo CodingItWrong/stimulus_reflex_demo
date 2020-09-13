bbefbbcd6efc839b1edeeeef81329b7b2a578c0e
Josh Justice <me@codingitwrong.com>
2020-09-13 07:23:34 -0400

    Move Create to SR

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,15 +3,4 @@ class TodosController < ApplicationController
     @todos = Todo.all
     @todo = Todo.new
   end
-
-  def create
-    Todo.create!(todo_params)
-    redirect_to todos_path
-  end
-
-  private
-
-  def todo_params
-    params.require(:todo).permit(:name)
-  end
 end
--- app/reflexes/todo_reflex.rb
+++ app/reflexes/todo_reflex.rb
@@ -1,6 +1,16 @@
 class TodoReflex < ApplicationReflex
+  def create
+    Todo.create!(todo_params)
+  end
+
   def delete
     id = element.dataset[:id]
     Todo.find(id).destroy!
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name)
   end
 end
